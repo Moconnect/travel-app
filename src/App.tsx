50fcee08019a0f5c15f75e8a4050490de387b946
Moses Ekpenisi <moses.ekpenisi@edu.fh-kaernten.ac.at>
2024-04-04 13:14:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import BannerHeader from './components/BannerHeader'
+import MainCard from './components/MainCard'
+import BannerSpace from './components/BannerSpace'
+
+const App = () => {
+  return (
+    <div>
+      <BannerHeader/>
+      <MainCard/>
+      <BannerSpace/>
+    </div>
+  )
+}
+
+export default App
+
